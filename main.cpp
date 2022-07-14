1f91de41ed43a3463084850c6d2cddcdfafac6c6
egor s <savushkin.egor.00027@gmail.com>
2022-07-14 14:05:16 +0300

    rezero

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "hey\n";
-
-    return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "../extern/cpp-terminal/cpp-terminal/base.hpp"
+#include "../extern/cpp-terminal/cpp-terminal/input.hpp"
+#include "../extern/cpp-terminal/cpp-terminal/window.hpp"
+#include <iostream>
+
+int main() {
+
+    std::cout << "hey\n";
+
+    return 0;
+}
